8295c3a529e3a6614bdaac4b5099ed4bb79ece53
CodeChd <Drowzyking1126@gmail.com>
2023-06-08 08:42:47 +0800

    Map Changes

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,32 @@
 import HeroComponent from "@/components/HeroComponent";
 import Identity from "@/components/Identity";
-import Mapa from "@/components/Map";
 import Mission from "@/components/Mission";
 import Visit from "@/components/Visit";
 import { Toaster } from "react-hot-toast";
+import Map from "@/Loc/Map"
+
+const DEFAULT_CENTER = [14.7089628  , 121.099202420]
+
+const Mapa = () => {
+  return (
+    
+    <Map width="500" height="100" center={DEFAULT_CENTER} zoom={12}>
+    {({ TileLayer, Marker, Popup }) => (
+      <>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="&copy; <a href=&quot;http://cjfrancisco.vercel.app&quot;>CJ</a> Developer"
+        />
+        <Marker position={DEFAULT_CENTER}>
+          <Popup>
+          Jesus Christ Yesterday Today and Forever Church.
+          </Popup>
+        </Marker>
+      </>
+    )}
+  </Map>
+  )
+}
 
 const about = () => {
   return (
